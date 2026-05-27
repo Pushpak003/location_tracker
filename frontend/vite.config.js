28f9132f231bf89c8c7f968e7061af302cd4976c
Pushpak003 <pushpakpathe63@gmail.com>
2026-05-27 15:14:55 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,24 @@
-import { defineConfig } from "vite";
+import {  defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit:
+      2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          mapbox: [
+            "mapbox-gl",
+          ],
+          socket: [
+            "socket.io-client",
+          ],
+          turf: [
+            "@turf/turf",
+          ],
+        },
+      },
+    },
+  },
 });
